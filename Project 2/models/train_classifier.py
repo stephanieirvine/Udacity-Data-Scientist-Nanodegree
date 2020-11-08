c0d3cf97d7a569c39bcb090a8bc75587baf4c2f0
Stephanie Irvine <54407746+stephanieirvine@users.noreply.github.com>
2020-11-08 22:15:04 +0000

    Update train_classifier.py

--- Project 2/models/train_classifier.py
+++ Project 2/models/train_classifier.py
@@ -118,7 +118,7 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
+        X, Y = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
@@ -128,7 +128,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
