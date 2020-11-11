c0c697fee9e7bce812a1b531315d2196dd2f5009
Stephanie Irvine <54407746+stephanieirvine@users.noreply.github.com>
2020-11-11 08:23:22 +0000

    updating path

--- Project 2/app/run.py
+++ Project 2/app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('disaster_messages', engine)
 
 # load model
-model = joblib.load("./models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
